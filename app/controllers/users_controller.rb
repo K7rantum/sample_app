1a08f46c7f6975a913c9dab3b90b73dc2a54fd4f
Kyle Trantum <ktrantum@gmail.com>
2011-10-19 01:15:14 -0700

    Done with user edit/update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
 
+  def index
+	@title = "All users"
+	@users = User.paginate(:page => params[:page])
+  end
+  
   def show
     @user  = User.find(params[:id])
 	@title = @user.name
-  end
+  end # show
 
   def new
 	@user  = User.new
     @title = "Sign up"
-  end
+  end # new
   
   def create
 	@user = User.new(params[:user])
@@ -22,4 +30,40 @@ class UsersController < ApplicationController
 	end
   end # create
   
+  def edit
+    @title = "Edit user"
+  end # edit
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end # update
+  
+  def destroy
+	User.find(params[:id]).destroy
+	flash[:success] = "User destroyed."
+	redirect_to users_path
+  end # destroy user (admin only)
+  
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end # authenticate
+	
+	def correct_user
+	  @user = User.find(params[:id])
+	  redirect_to(root_path) unless current_user?(@user)
+	end
+ 
+	def admin_user
+	  redirect_to(root_path) current_user.admin?
+	end
+	
 end
