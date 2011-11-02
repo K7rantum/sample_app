db80ba928933a937faf7b8909ead834399b10c50
Kyle Trantum <ktrantum@gmail.com>
2011-11-02 14:12:27 -0700

    Fixed file structure

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fd092cbd0d1ca6f055e7b150cfce3acd12d5935043a8022ab9b9735843b7e0e12512ab9e023cefbab1aefc2ff9955187a05edccc342475d60d622207cc928083'
---
+++ sample_app/spec/spec_helper.rb
@@ -0,0 +1,11 @@
+# something goes here
+# must find code
+# and replace here
+RSpec.configure do |config|
+  # stuff also goes in
+  # between here, must
+  # find code for this
+  def test_sign_in(user)
+    controller.sign_in(user)
+  end
+end
