6ba85ffc22acc2d089eb0e51466b35c0a6dc01d5
Kyle Trantum <ktrantum@gmail.com>
2011-11-15 12:47:45 -0800

    edited changed to specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,39 +6,40 @@ describe PagesController do
   before(:each) do
     @base_title = "Ruby on Rails Tutorial Sample App"
 
-  describe "GET 'home'" do
-    
-    describe "when not signed in" do
-    
-      before(:each) do
-        get :home
-      end
+    describe "GET 'home'" do
       
-      it "should be successful" do
-        response.should be_success
-      end
+      describe "when not signed in" do
       
-      it "should have the right title" do
-        response.should have_selector("title",
-                                      :content => "#{@base_title} | Home")
-      end
-    end # not signed in
-    
-    describe "when signed in" do
+        before(:each) do
+          get :home
+        end
+        
+        it "should be successful" do
+          response.should be_success
+        end
+        
+        it "should have the right title" do
+          response.should have_selector("title",
+                                        :content => "#{@base_title} | Home")
+        end
+      end # not signed in
       
-      before(:each) do
-        @user = test_sign_in(Factory(:user))
-        other_user = Factory(:user, :email => Factory.next(:email))
-        other_user.follow!(@user)
-      end
-      
-      it "should have the right follower / following counts" do
-        get :home
-        response.should have_selector("a", :href    => following_user_path(@user),
-                                           :content => "0 following")
-        response.should have_selector("a", :href    => followers_user_path(@user),
-                                           :content => "1 follower")
-      end
-    end # signed in
-  end # get home
+      describe "when signed in" do
+        
+        before(:each) do
+          @user = test_sign_in(Factory(:user))
+          other_user = Factory(:user, :email => Factory.next(:email))
+          other_user.follow!(@user)
+        end
+        
+        it "should have the right follower / following counts" do
+          get :home
+          response.should have_selector("a", :href    => following_user_path(@user),
+                                             :content => "0 following")
+          response.should have_selector("a", :href    => followers_user_path(@user),
+                                             :content => "1 follower")
+        end
+      end # signed in
+    end # get home
+  end
 end
